ffeacba51ae1be36674e4eeffe61089244a7c64d
yogik23 <61941105+yogik23@users.noreply.github.com>
2024-07-28 01:58:16 +0700

    Create gaian.js

---
+++ gaian.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+const fs = require('fs');
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+;(async () => {
+    try {
+        console.log('BOT Auto SendCHAT GAIAN By [Peking404XYogiPrt666]\n\n');
+        const addressList = await fs.readFileSync('keyword.txt', 'utf-8');
+        const addressListArray = await addressList.split('\n');
+
+        for (let index = 11; index < addressListArray.length; index++) {
+            const Wallet = addressListArray[index];
+            console.log("Content Chat: " + Wallet + "\n");
+
+            try {
+                const response = await axios.post(
+                    'https://NodeIdGaiaMu.us.gaianet.network/v1/chat/completions',
+                    {
+                        'messages': [
+                            {
+                                'role': 'system',
+                                'content': 'You are a helpful assistant.'
+                            },
+                            {
+                                'role': 'user',
+                                'content': `${Wallet}`
+                            }
+                        ]
+                    },
+                    {
+                        headers: {
+                            'accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                );
+
+                console.log("Response: [" + response.data.choices[0].message.content + "]\n");
+                console.log("Sabar 15 Menit \n\n");
+                await delay(900000);
+            } catch (postError) {
+                console.error("Error during axios post: ", postError);
+            }
+        }
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+})();
